250b4d8c6e7f7dd1ccd2348753a1630834c3442a
Zozulya Maksim <soundmax15041987@gmail.com>
2025-03-06 14:50:24 +0300

    Changed functions to work with objects and added getting date from element input

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,13 @@
 'use strict';
 
 // 1.Создаем массив для хранения наших задач
-const todoList = ['Купить еду','Посмотреть фильм'];
+const todoList = [{
+  name: 'Купить еду',
+  dueDate: '2025-03-06'
+},{
+  name: 'Посмотреть фильм',
+  dueDate: '2025-03-06'
+}];
 
 // Отображаем список на странице
 renderTodo();
@@ -9,21 +15,18 @@ renderTodo();
 function renderTodo() {
 
   let todoListHTML = '';
-  
-  // Проходимся по всем элементам массива и создаем html для каждой задачи
+   // Проходимся по всем элементам массива и создаем html для каждой задачи 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
+    const todoObject = todoList[i];
+    const {name, dueDate} = todoObject;
     const html = 
-    `<p>${todo}
+    `<p>${name} ${dueDate}
      <button onclick="
      todoList.splice(${i}, 1);
      renderTodo();
      ">Delete</button></p>`; // удалям 1 значение из списка и обновляем список с помощью функции renderTodo()
-     
     todoListHTML += html;
-  }
-    console.log(todoListHTML);
-  
+  }  
   document.querySelector('.js-todo').innerHTML = todoListHTML;
 }
 
@@ -32,10 +35,14 @@ function renderTodo() {
 function addTodo() {
   const inputElement = document.querySelector('.js-input');
   const name = inputElement.value;
-  todoList.push(name);
 
-  // 3. Выводим на консоль
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-date'); // получаем  дату
+  const dueDate = dateInputElement.value;
+
+  todoList.push({ // добавляем обьект в массив с задачей и датой
+    name,
+    dueDate
+  });
 
   // Очищаем поле после  ввода текста
   inputElement.value = '';
